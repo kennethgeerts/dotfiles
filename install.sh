ef19d950e6b1d0226abd8bf945946c09e6d9b886
Kenneth Geerts <kenneth@sofuto.be>
2020-08-02 19:48:47 +0200

    Delete obvious comment

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.serve
 
 # Prezto
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-setopt EXTENDED_GLOB # Why doesn't this work?
+setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done

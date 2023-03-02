0099aeb16a285c1c2d11573022705117c144f875
Kenneth Geerts <kenneth@sofuto.be>
2023-03-02 10:28:07 +0100

    No error if not connected to local WiFi

--- SwiftBar/solar.30s.sh
+++ SwiftBar/solar.30s.sh
@@ -2,20 +2,26 @@
 
 import asyncio
 import goodwe
+import os
 
 async def get_runtime_data():
     ip_address = '192.168.0.14'
     inverter = await goodwe.connect(ip_address)
     return await inverter.read_runtime_data()
 
-data = asyncio.run(get_runtime_data())
-ppv = data['ppv1']
-load = data['house_consumption']
-soc = data['battery_soc']
-day = data['e_day']
-total = data['e_total']
+ssid = os.popen("/Sy*/L*/Priv*/Apple8*/V*/C*/R*/airport -I | awk '/ SSID:/ {print $2}'").read().strip()
 
-print(":sun.max:%.0fW :bolt:%sW :bolt.fill.batteryblock:%.0f%% | size = 11" % (ppv, load, soc))
-print("---")
-print("Day: %.2fkWh" % day)
-print("Total: %.2fkWh" % total)
+if ssid == 'SofutoNET':
+    data = asyncio.run(get_runtime_data())
+    ppv = data['ppv1']
+    load = data['house_consumption']
+    soc = data['battery_soc']
+    day = data['e_day']
+    total = data['e_total']
+
+    print(":sun.max:%.0fW :bolt:%sW :bolt.fill.batteryblock:%.0f%% | size = 11" % (ppv, load, soc))
+    print("---")
+    print("Day: %.2fkWh" % day)
+    print("Total: %.2fkWh" % total)
+else:
+    print('')

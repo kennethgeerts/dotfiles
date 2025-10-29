0a3e06a19d40e6f0ecb490b38020129c04d1140d
Kenneth Geerts <kenneth@sofuto.be>
2025-10-29 15:19:04 +0100

    No animation on indent guides

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,15 @@
 return {
-  {
-    "catppuccin/nvim",
-    opts = {
-      flavour = "frappe", -- latte, frappe, macchiato, mocha
-      transparent_background = true,
-    },
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  },
+	{
+		"catppuccin/nvim",
+		opts = {
+			flavour = "frappe", -- latte, frappe, macchiato, mocha
+			transparent_background = true,
+		},
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin",
+		},
+	},
 }
---
+++ config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,15 @@
+return {
+	{
+		"folke/snacks.nvim",
+		opts = {
+			indent = {
+				animate = {
+					enabled = false,
+				},
+			},
+			scroll = {
+				enabled = false,
+			},
+		},
+	},
+}
